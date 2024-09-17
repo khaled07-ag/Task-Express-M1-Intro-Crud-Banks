2a9fdc061508ee4f8e129d1396788b11f6fc5fb3
Khaled Alswayed <khaled-kk01@hotmail.com>
2024-09-17 10:03:39 +0300

    completed with routes

--- app.js
+++ app.js
@@ -1,42 +1,10 @@
 const express = require("express");
 const app = express();
 const accounts = require("./accounts");
+const accountsRouter = require("./apis/accounts.routes")
 app.use(express.json());
+app.use("/api",accountsRouter);
 
-app.get("/accounts", (req, res) => {
-  res.status(200).json(accounts);
-});
-app.post("/create", (req, res) => {
-  const newAcc = {
-    id: req.body.id,
-    username: req.body.username,
-    funds: req.body.funds,
-  };
-  accounts.push(newAcc);
-  res.status(201).json({
-    message: "Account Added!",
-    data: accounts,
-  });
-});
-app.delete("/delete/:id",(req,res)=>{
-  const {id} = req.params;
-  const updatedAcc = accounts.filter((account)=>{
-    if(account.id != id){
-      return true;
-    }
-    
-  })
-  return res.status(200).json({data: updatedAcc});
-})
-app.put("/update/:id",(req,res)=>{
-  const {id} = req.params;
-  const accFound = accounts.find((accounts)=>{
-    if(accounts.id == id){
-      return true;
-    }
-  })
-  
-})
 app.listen(8000, () => {
   console.log("first");
 });
